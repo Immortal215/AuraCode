d68cc7dc391edf50bad032bf849ddb9df88b4e85
Immortal215 <sharulshah@icloud.com>
2025-06-27 17:52:32 -0400

    Initial Commit

---
+++ AuraCode/AuraCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraCodeApp.swift
+//  AuraCode
+//
+//  Created by Sharul Shah on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct AuraCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
